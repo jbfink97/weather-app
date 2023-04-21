9f24cd7ada1824b15f3f6dbb20aa05bccaba6c52
Johnny <johnnyfink97@gmail.com>
2023-04-21 16:44:20 -0400

    set up basic HTML, CSS, and JS files. Began working on JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const content = document.getElementById('main');
+
+const form = document.createElement('form');
+const searchBarDiv = document.createElement('div');
+searchBarDiv.id = 'searchBarDiv';
+const input = document.createElement('input');
