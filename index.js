4676cbe3806a2eafff055f220248abb6309b3366
Johnny <johnnyfink97@gmail.com>
2023-04-21 20:16:51 -0400

    worked on more styling

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const form = document.createElement('form');
 const searchBarDiv = document.createElement('div');
 searchBarDiv.id = 'searchBarDiv';
 const input = document.createElement('input');
+input.type = 'text';
+input.id = 'Location';
+input.placeholder = 'Search city or postal code';
+
+const submitBtn = document.createElement('button');
+submitBtn.id = 'submitBtn';
+
+form.append(input, submitBtn)
+searchBarDiv.append(form);
+content.append(searchBarDiv);
+
+const weatherDiv = document.createElement('div');
+content.append(weatherDiv);
+weatherDiv.id = 'weatherDiv';
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    console.log('click');
+})
+
